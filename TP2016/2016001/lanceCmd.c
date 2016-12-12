57267b85b6c1de7c5cd0e884730331a34f567f74
Pascal Emile Fares <pfares@cofares.net>
2016-12-12 22:30:01 +0200

    Exec et fork

---
+++ TP2016/2016001/lanceCmd.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[], char **env){
+    
+    char **myargv;
+    int i;
+    int pid;
+
+    /**
+    myargv=(char **)malloc(argc * sizeof(char *));
+    
+    for (i=1;i<argc;i++) {
+       myargv[i-1]=argv[i]; 
+       printf("%s %d",myargv[i-1], i-1);
+    }
+    myargv[argc-1]=NULL;
+    printf("%s %d\n",myargv[argc-1], argc-1);
+
+    // argv+1 remplace tout ceci et est suffisant
+    */
+
+    if ((pid =fork()) == 0) {
+        //Je suis le fils
+        //execve (argv[1], myargv, env);
+        printf("chez le fils... avant execvp %d", getpid());
+        execvp (argv[1], argv+1);
+        perror("\n--------------si ici pas OK---\n");
+    }
+    /** attendre la fin du fils */
+    wait();
+
+ /** UNIQUEMENT POUR COMPARER, sinon inutile pour l'exercice en lui même 
+  * constater la différence entre execv et execvp
+  * execvp comme bash recharche le programme en tenant compte du path
+  * execv 
+  */
+    printf("\n%d mon fils est %d\n", getpid(),pid);
+    execv (argv[1], argv+1);
+    perror("\n execv du père...");
+    
+}
