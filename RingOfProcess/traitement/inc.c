29bef3cf4eec741691294636cf53a2238ac9aca8
Pascal Emile Fares <pfares@cofares.net>
2017-01-18 15:59:01 +0200

    Préparation bis

--- RingOfProcess/traitement/inc.c
+++ RingOfProcess/traitement/inc.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 #define MAXBUFF 255
 
 int traitement (int a) {
@@ -8,10 +8,27 @@ int traitement (int a) {
 
 int main () {
     
-	int x; 
+	int x;
+        char c;
+        int res;
 	while(1) {
-    		scanf("%d",&x );
-    		printf("%d",traitement(x));
+    		res = scanf("%d",&x);
+                if (res==EOF) {
+                   write(2,"Do nothing c'est ce n'est pas bon!",36);
+                   exit(1);
+                } else if (res==1){ 
+                  //%d a résussi
+    		  printf("%d\n",traitement(x));
+                  fflush(stdout);
+                } else {
+                   //Une erreure  ou fin de ligne on essaye de réparrer
+                   scanf("%c",&c);
+                   if (c=='\n') {
+                         //On trasmet new line pas de mal 
+                         printf("%c",c);
+                         fflush(stdout);
+                   } else write(2,"Erreure ...", 12);
+                } 
 	}
 
 }
