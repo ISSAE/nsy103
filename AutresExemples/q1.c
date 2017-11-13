ade81133373875e19cdf359fa5254ae7eb689671
Pascal Fares <pascal.fares@cofares.net>
2017-11-13 12:26:16 +0200

    Test de clang-format

--- AutresExemples/q1.c
+++ AutresExemples/q1.c
@@ -1,11 +1,10 @@
 void main(int argc, char **argv) {
-      int x=0;
+  int x = 0;
 
-      printf("%d --x est: %d\n", getpid(), x);
-      fork();
-      x++;
-      fork();
-      x++;
-      printf("%d --x est: %d adresse est %p\n", getpid(), x, &x);
-
+  printf("%d --x est: %d\n", getpid(), x);
+  fork();
+  x++;
+  fork();
+  x++;
+  printf("%d --x est: %d adresse est %p\n", getpid(), x, &x);
 }
