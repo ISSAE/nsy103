cf568484af1fbd72392cc538a144c6d4f75752cb
Pascal Fares <pascal.fares@cofares.net>
2017-12-04 12:02:42 +0200

    Socket exemples

---
+++ Sockets/Exemples/echo/echoSrv.c
@@ -0,0 +1,45 @@
+/*Entêtes nécessaires*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "../helper/helperSO.h"
+#include "serviceEcho.h"
+#include <pthread.h>
+#include <stdint.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+
+    //char str[100];
+    int listen_fd, comm_fd;
+    
+    if (argc==1) {
+        listen_fd = bindedSocket(NULL, "2023", SOCK_STREAM, NULL);
+    }
+    else listen_fd = bindedSocket(NULL, argv[1], SOCK_STREAM, NULL);
+
+    if (listen(listen_fd, 10) < 0) {
+        perror("listen");
+        exit(1);
+    }
+    while (1) {
+        comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+        if (comm_fd < 0) {
+            perror("accept");
+            exit(2);
+        }
+        
+        if (fork() ==0) {
+            printf("[INFO] un service fils pid=%d père=%d\n",getpid(), getppid());
+            //Le fils fait le service et se termine
+            service((void *)(intptr_t)comm_fd);
+            exit(0);
+        } 
+    }
+    close(listen_fd);
+}
