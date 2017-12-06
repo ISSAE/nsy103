b35967325f18c617dcfb0297a33f3ea920f7faeb
Pascal Fares Domicile <pascal.fares@cofares.net>
2017-12-06 11:08:35 +0200

    Remove Useless dirs

--- unpv12e/libxti/xti_rdwr.c
+++
@@ -1,7 +0,0 @@
-#include	"unpxti.h"
-
-int
-xti_rdwr(int fd)
-{
-	return(ioctl(fd, I_PUSH, "tirdwr"));
-}
--- unpv12e/traceroute/sig_alrm.c
+++
@@ -1,7 +0,0 @@
-#include	"trace.h"
-
-void
-sig_alrm(int signo)
-{
-	return;		/* just interrupt the recvfrom() */
-}
