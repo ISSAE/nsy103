cfc3f6424ed12bd7f36bd8997058de8b732772ee
Pascal Fares <pascal.fares@cofares.net>
2017-12-06 16:05:12 +0200

    Présentaion Problèmes besoin de section critique ou accès atomique

---
+++ BaseProgLinux/Threads/concurence.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+/**
+ * Tableau des id de thread créés
+ */
+pthread_t tid[10];
+int x=0;
+
+//void* equivalent a ANY
+void* incX(void *arg)
+{
+    unsigned int i = 0;
+    int xavant=x;
+    pthread_t id = pthread_self();
+
+    printf("X avant %lu %d\n", id, xavant);
+    for (i=0; i< 10000; i++) {
+        //printf("X ++ %d",x);
+        x +=1;
+    }
+    printf("X apres %lu  normalement %d %d\n", id, x, xavant+10000);
+}
+
+int main(void)
+{
+    int i = 0;  
+    int err;
+    int *ptr[10];
+    
+    while(i < 10)
+    {
+        err = pthread_create(&(tid[i]), NULL, &incX, &i);
+        if (err != 0)
+            printf("\nPas réussi à créer le thread :[%s]", strerror(err));
+        else
+            printf("\n création de thread ok %ld\n",tid[i]);
+
+        i++;
+    }
+
+    for (i=0; i<10; i++) {
+        //printf("join %lu\n",tid[i]);
+         pthread_join(tid[i], (void**)&(ptr[i]));
+    }
+    
+
+    printf("valeur de x après 10*10000 est %d\n",x);
+
+    return 0;
+}
